31a6670699e71753e0a55b57b38ef625f95fb420
Lew Andrade <andrade.lew@gmail.com>
2021-04-19 04:24:15 -0300

    Implemented json encoding

--- controllers/front.go
+++ controllers/front.go
@@ -1,10 +1,19 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 func RegisterControllers() {
 	uc := newUserController()
 
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc)
+}
+
+func EncodeResponseAsJSON(data interface{}, writer io.Writer) {
+	enc := json.NewEncoder(writer)
+	_ = enc.Encode(data)
 }
