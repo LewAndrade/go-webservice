27337a88461bfe6ded73bf763d86c9e10aff6303
Lew Andrade <andrade.lew@gmail.com>
2021-04-19 04:25:08 -0300

    Created user model

--- models/user.go
+++ models/user.go
@@ -1,9 +1,13 @@
 package models
 
+import (
+	"fmt"
+)
+
 type User struct {
-	ID        int
-	FirstName string
-	LastName  string
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 var (
@@ -16,8 +20,41 @@ func GetUsers() []*User {
 }
 
 func AddUser(user User) (User, error) {
-	user.ID = nextID
+	if user.Id != 0 {
+		return User{}, fmt.Errorf("new User must not include id or it must be set to zero")
+	}
+	user.Id = nextID
 	nextID++
 	users = append(users, &user)
 	return user, nil
+}
+
+func GetUserById(id int) (User, error) {
+	for _, user := range users {
+		if user.Id == id {
+			return *user, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("user with ID '%v' not found", id)
+}
+
+func UpdateUser(user User) (User, error) {
+	for i, candidate := range users {
+		if candidate.Id == user.Id {
+			users[i] = &user
+			return user, nil
+		}
+	}
+	return User{}, fmt.Errorf("user with ID '%v' not found", user.Id)
+}
+
+func RemoveUserByID(id int) error {
+	for i, user := range users {
+		if user.Id == id {
+			users = append(users[:i], users[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("user with ID '%v' not found", id)
 }
